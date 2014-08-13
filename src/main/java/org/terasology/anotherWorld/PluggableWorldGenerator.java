372ff47c4b91e0bc86c875750e0f057640851f25
Josharias <josh.owie@gmail.com>
2014-08-12 18:05:29 -0700

    Use facets to get temperature and humidity data for rendering.  Remove unused fog.

--- src/main/java/org/terasology/anotherWorld/PluggableWorldGenerator.java
+++ src/main/java/org/terasology/anotherWorld/PluggableWorldGenerator.java
@@ -169,21 +169,6 @@ public abstract class PluggableWorldGenerator implements WorldGenerator {
     }
 
     @Override
-    public float getFog(float x, float y, float z) {
-        return 0.5f;
-    }
-
-    @Override
-    public float getTemperature(float x, float y, float z) {
-        return 0.5f;
-    }
-
-    @Override
-    public float getHumidity(float x, float y, float z) {
-        return 0.5f;
-    }
-
-    @Override
     public World getWorld() {
         return world;
     }
