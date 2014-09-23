8ede8ac0d13bba9f667d9c0550c318e2f76088c8
Sebastian Hartte <sebastian@hartte.de>
2014-09-23 15:40:41 +0200

    Plant systems now use the biome temperature/humidity for deciding whether to grow (change to LocalParameters), due to the high performance impact of perlin noise. Added biome id mapping synchronisation to the network protocol. Refactored several interfaces to remove access to world generation in non-authoritative mode and removed the RemoteWorldGenerator since it's no longer needed.

--- src/main/java/org/terasology/anotherWorld/WorldLocalParameters.java
+++ src/main/java/org/terasology/anotherWorld/WorldLocalParameters.java
@@ -3,6 +3,7 @@ package org.terasology.anotherWorld;
 import org.terasology.math.Region3i;
 import org.terasology.math.Vector3i;
 import org.terasology.world.WorldProvider;
+import org.terasology.world.biomes.Biome;
 import org.terasology.world.generation.Region;
 import org.terasology.world.generation.facets.SurfaceHumidityFacet;
 import org.terasology.world.generation.facets.SurfaceTemperatureFacet;
@@ -21,23 +22,11 @@ public class WorldLocalParameters implements LocalParameters {
 
     @Override
     public float getTemperature() {
-        float temperature = 0.5f;
-        Region region = worldProvider.getWorldData(Region3i.createFromCenterExtents(location, 1));
-        if (region != null) {
-            SurfaceTemperatureFacet surfaceTemperatureFacet = region.getFacet(SurfaceTemperatureFacet.class);
-            temperature = surfaceTemperatureFacet.getWorld(location.x, location.z);
-        }
-        return temperature;
+        return worldProvider.getBiome(location).getTemperature();
     }
 
     @Override
     public float getHumidity() {
-        float humidity = 0.5f;
-        Region region = worldProvider.getWorldData(Region3i.createFromCenterExtents(location, 1));
-        if (region != null) {
-            SurfaceHumidityFacet surfaceHumidityFacet = region.getFacet(SurfaceHumidityFacet.class);
-            humidity = surfaceHumidityFacet.getWorld(location.x, location.z);
-        }
-        return humidity;
+        return worldProvider.getBiome(location).getHumidity();
     }
 }
